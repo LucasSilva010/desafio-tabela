e91dfbb14a65ec6591b8a6122d6bf4749b7eaea5
Lucas Silva <lucascitty2@gmail.com>
2023-07-12 16:08:37 -0300

    Implementação da tabela. Inclusão de dados a partir de um arquivo php externo e manipulação destes dados dentro da tabela

--- tabela.php
+++ tabela.php
@@ -7,18 +7,30 @@
 
    <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12">
-  
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-12">
-
+<div class="container ">
+  <div class="row ">
+    <div class="col-lg-12 align-items-center">
+    <?php include './data.php' ?>
+      <table class="table table-bordered table-striped table-hover">
+        <tr>
+          <th>Nome</th>
+          <th>E-mail</th>
+          <th>Função</th>
+        </tr>
+        <?php foreach ($usuarios as $usuario) : ?>
+          <tr>
+            <td><?= $usuario['nome'] ?></td>
+            <td><?= $usuario['email'] ?></td>
+            <td><?= $usuario['funcao'] ?></td>
+          </tr>
+        <?php endforeach ?>
+      </table>
     </div>
   </div>
 </div>
