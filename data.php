b30ac66baad365cb5d57e384dbc41ec239e482d8
Lucas Silva <lucascitty2@gmail.com>
2023-07-12 16:22:07 -0300

    Adição de todos os 15 usuários fictícios

---
+++ data.php
@@ -0,0 +1,83 @@
+<?php
+  $usuarios = [
+    [
+        'nome' => 'João Silva',
+        'email' => 'joao.silva@example.com',
+        'funcao' => 'Desenvolvedor'
+    ],
+    [
+        'nome' => 'Maria Santos',
+        'email' => 'maria.santos@example.com',
+        'funcao' => 'Designer'
+    ],
+    [
+        'nome' => 'Pedro Almeida',
+        'email' => 'pedro.almeida@example.com',
+        'funcao' => 'Analista de Dados'
+    ],
+
+    [
+      'nome' => 'Alessandra Loia',
+      'email' => 'ale.loia@example.com',
+      'funcao' => 'Professora de Inglês'
+    ],
+
+    [
+      'nome' => 'Lucas Silva',
+      'email' => 'lucas.ssilva@example.com',
+      'funcao' => 'Desenvolvedor Front-End'
+    ],
+
+    [
+      'nome' => 'Ricardo Machado',
+      'email' => 'ricardo_m@example.com',
+      'funcao' => 'Eletricista'
+    ],
+
+    [
+      'nome' => 'Robson Rosa',
+      'email' => 'robsonrosa.s@example.com',
+      'funcao' => 'Advogado'
+    ],
+    [
+      'nome' => 'Gabriel Santos',
+      'email' => 'gab.santos01@example.com',
+      'funcao' => 'Contador'
+    ],
+    [
+      'nome' => 'Bianca Barros',
+      'email' => 'bianca_barros@example.com',
+      'funcao' => 'Gerente'
+    ],
+    [
+      'nome' => 'Matheus Miranda',
+      'email' => 'matmiranda2@example.com',
+      'funcao' => 'Técnico em TI'
+    ],
+    [
+      'nome' => 'ALberto Carneiro',
+      'email' => 'alber.carneiro023@example.com',
+      'funcao' => 'Encanador'
+    ],
+    [
+      'nome' => 'Deide Costa',
+      'email' => 'dei.decosta@example.com',
+      'funcao' => 'Faxineira'
+    ],
+    [
+      'nome' => 'Jacinto Pinto',
+      'email' => 'ja.cinto_pinto@example.com',
+      'funcao' => 'Desenvolvedor'
+    ],
+    [
+      'nome' => 'Elma Maria',
+      'email' => 'el.ma-maria@example.com',
+      'funcao' => 'Doméstica'
+    ],
+    [
+      'nome' => 'Juriscleidson Abroba',
+      'email' => 'juris.abroba@example.com',
+      'funcao' => 'Dono de tudo'
+    ],
+];
+?>
